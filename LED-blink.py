d6e806748dd692d11f0d975ba1cdda8aa3751022
Dan <dan.m.larsson@gmail.com>
2018-03-24 17:14:06 +0000

    Some comments and cleaning of tabs

--- LED-blink.py
+++ LED-blink.py
@@ -15,46 +15,53 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(LED, GPIO.OUT)
 GPIO.output(LED, GPIO.LOW)
 
+
+# Main loop, checks if the port is down and tries to do a new connection
 def main():
-	s = connect()
+   s = connect()
 
-	while 1:
-		try:
-			data = s.recv(1024)
-			if len(data) == 0:
-				print('Lost connection, trying to reconnect')
-				s.close()
-				s = connect()
-			# print data.strip()
-			blink()
-		except socket.error:
-			print('Lost connecttion, reconnecting')
-			s = connect()
+   while 1:
+      try:
+         data = s.recv(1024)
+         if len(data) == 0:
+            print('Lost connection, trying to reconnect')
+            s.close()
+            s = connect()
+            blink()
+            # Uncomment the line below if you like to see the raw data.
+            # print data.strip()
+
+      except socket.error:
+         print('Lost connecttion, reconnecting')
+         s = connect()
 
 
+# Well it blinks, a short one.
 def blink():
-	# print 'Blink'
-	GPIO.output(LED, GPIO.HIGH)
-	time.sleep(LIGHT_ON)
-	GPIO.output(LED, GPIO.LOW)
+   # print('Blink')
+   GPIO.output(LED, GPIO.HIGH)
+   time.sleep(LIGHT_ON)
+   GPIO.output(LED, GPIO.LOW)
 
 
+# Do a connection to a socket, if the server is down wait 10 seconds and try again.
 def connect():
-	while 1:
-		try:
-			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-			s.connect((HOST, PORT))
-			print("Connected to dump1090 on %s:%i" % (HOST, PORT))
-			return s
-		except socket.error:
-			print("Cant connect to dump1090 on %s:%i, trying again in 10" % (HOST, PORT))
-			time.sleep(10)
+   while 1:
+      try:
+         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+         s.connect((HOST, PORT))
+         print("Connected to dump1090 on %s:%i" % (HOST, PORT))
+         return s
+      except socket.error:
+         print("Cant connect to dump1090 on %s:%i, trying again in 10" % (HOST, PORT))
+         time.sleep(10)
 
 
+# Run the code untill CRTL-C is pressed. Then resets the GPIO-pin and exits nicely.
 if __name__ == '__main__':
-	try:
-		main()
-	except KeyboardInterrupt:
-		GPIO.cleanup()
-		print 'Exiting!'
-		exit()
+   try:
+      main()
+   except KeyboardInterrupt:
+      GPIO.cleanup()
+      print 'Exiting!'
+      exit()
